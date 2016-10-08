5f2d3e6cf61ecc5fa6273aab27da3fe874315cc2
Yogesh Gupta <yogesh201996@gmail.com>
2016-10-08 14:41:57 +0530

    Basic Use

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
 var express = require('express'),
     fs = require('fs'),
     request = require('request'),
-    cheerio = require('cheerio');
+    cheerio = require('cheerio'),
+    httpProxyAgent = require('http-proxy-agent');
 
 var app = express();
 
 app.get('/scrape',function(req,res){
     //The url where we will scrape from.Request is sent using request required above.
+    var url = "http://www.imdb.com/title/tt0111161/";
+    var url1 = "http://www.imdb.com/title/tt0137523/";
+    // To be used when working behind a proxy server.
 
-    var url = "http://www.imdb.com/title/tt1229340/";
+    // var agent = new httpProxyAgent("http://172.31.1.4:8080");
+    request({
+        uri : url1
+        // agent : agent
+    },
+    function(error, response, html){
+        if(!error && response.statusCode == 200){
+            var $ = cheerio.load(html);
+            var title, release ,rating;
 
-    request(url, function(error, responseCode, html){
-        if(!error){
+            var json = {
+                title : "",
+                release : "",
+                rating : ""
+            };
 
+            var detailsHeader = $(".title_wrapper").children("h1");
+
+            var title = detailsHeader.text();
+
+            json.title = title;
+            console.log(title);
+            res.send(html);
+        }
+        else{
+            console.log("Could not connect.");
         }
     });
-    res.send("Hello User\n");
+
 });
 
 app.listen(8000);
